f0ae818d4f07c0cfe536c740fc042a0d4f0ea76d
Napaorn <148393919+Napaorn@users.noreply.github.com>
2024-03-27 16:21:35 +0700

    Burnout syndromes

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1711530949,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground6.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/tiledbackground7.png",
+		"images/sprite5-sheet0.png",
+		"images/tiledbackground8.png",
+		"images/tiledbackground9.png",
+		"images/tiledbackground10.png",
+		"images/over-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
